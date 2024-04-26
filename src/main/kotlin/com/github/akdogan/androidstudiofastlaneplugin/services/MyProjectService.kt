02980ceb56d3510f93416aaaecb796a74db93e3b
GitHub Action <action@github.com>
2024-04-26 07:30:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/akdogan/androidstudiofastlaneplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/akdogan/androidstudiofastlaneplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.akdogan.androidstudiofastlaneplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akdogan.androidstudiofastlaneplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
